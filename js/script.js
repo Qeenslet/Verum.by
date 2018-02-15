27570f5b865bdff7b2f24635e094979aed6824b5
Egor <egorgulidow@mail.ru>
2018-02-15 12:47:35 +0300

    валидация на 4й бутстрап

--- js/script.js
+++ js/script.js
@@ -3,25 +3,44 @@ var serverConnector =
 {
     send: function ()
     {
-        $.ajax({
-            url:"mailer.php",
-            data: $('#contactForm').serialize(),
-            dataType: "json",
-            method: 'post'
-        }).done(function(data) {
-            if (data.ok)
-            {
-                $("#contactForm")[0].reset();
-                $("#success").modal("show");
-            }
-            else
-            {
-                alert('К сожалению, у нас проблемы с почтовым сервером, сообщение не было отправлено');
-            }
+        var check = document.getElementById('contactForm');
+        if (check.checkValidity())
+        {
+            $.ajax({
+                url:"mailer.php",
+                data: $('#contactForm').serialize(),
+                dataType: "json",
+                method: 'post'
+            }).done(function(data) {
+                if (data.ok)
+                {
+                    $("#contactForm")[0].reset();
+                    $("#success").modal("show");
+                }
+                else
+                {
+                    alert('К сожалению, у нас проблемы с почтовым сервером, сообщение не было отправлено');
+                }
 
-        }).fail(function() {
-            alert('Не получилось :(');
-        });
+            }).fail(function() {
+                alert('Не получилось :(');
+            });
+        }
+        else
+        {
+            for (var i = 0; i < check.elements.length; i++)
+            {
+                var el = check[i];
+                if (el.hasAttribute('name'))
+                {
+                    if (!el.checkValidity())
+                    {
+                        el.classList.add('is-invalid');
+                    }
+                }
+            }
+        }
+
     }
 }
 
